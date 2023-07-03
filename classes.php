c0fab4d23a52716cdc7533498b472a2ec1f3e6dc
404Paolo <cpaolo852@gmail.com>
2023-07-03 17:05:30 +0800

    modified:   classes.php

--- classes.php
+++ classes.php
@@ -53,8 +53,10 @@
 
         public function addtoCart($id){
             global $inventory;
+
             if($inventory[$id]["stock"] >= 1){
-                $this->cart->items[$id] = $inventory[$id];
+                $this->cart->items[] = $id;
+                sort($this->cart->items);
                 $inventory[$id]["stock"] -= 1;
             }
 
@@ -65,8 +67,50 @@
         
         public function removefromCart($id){
             global $inventory;
-            unset($this->cart->items[$id]);
-            $inventory[$id]["stock"] += 1;
+            for($i = 0; $i < count($this->cart->items); $i++){
+                if($this->cart->items[$i] == $id){
+                    unset($this->cart->items[$i]);
+                    $found = true;
+                    $inventory[$id]["stock"] += 1;
+                    break;
+                }
+            }
+
+            if(!$found){
+                echo "Item not found";
+            }
+
+            
+        }
+
+        public function generateCartId(){
+            $base = 40;
+            $num = 0;
+            $items = array();
+            $len = count($this->cart->items);
+
+            foreach($this->cart->items as $n=>$id){
+                $num += ($id * pow($base, $len - ($n+1)));
+            }
+
+            return dechex($num);
+        }
+        
+        public function decryptCartId($num){
+            $num = hexdec($num);
+            $txt = $this->recurse($num);
+            return str_split($txt);
+        }
+
+        private function recurse($num){
+            $base = 40;
+            if($num <= $base){
+                return strval($num%$base);
+            }
+
+            else{
+                return strval($this->recurse($num/$base)).strval($num%$base);
+            }
         }
     }
 
@@ -77,9 +121,6 @@
         public function __construct($items = array(), $balance = 0){
             $this->items = $items;
             $this->balance = $balance;
-        }
-
-        public function generateID(){
         }
     }
 
