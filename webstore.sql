eceafee831b0eb217bf4edeb7e6cc133ac9d0b93
404Paolo <cpaolo852@gmail.com>
2023-07-08 13:05:26 +0800

    modified:   webstore.sql

--- webstore.sql
+++ webstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2023 at 11:27 AM
+-- Generation Time: Jul 08, 2023 at 07:04 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -32,55 +32,56 @@ CREATE TABLE `items` (
   `name` varchar(50) NOT NULL,
   `stock` int(5) NOT NULL,
   `price` int(5) NOT NULL,
-  `image` varchar(50) NOT NULL
+  `image` varchar(50) NOT NULL,
+  `description` varchar(80) DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`id`, `name`, `stock`, `price`, `image`) VALUES
-(0, '', 0, 0, ''),
-(1, 'Team Medallion', 1, 1000, 'assetsTeam_Medallion.png'),
-(2, 'Incubator', 100, 150, 'assetsIncubator_Limited.png'),
-(3, 'Super Incubator', 50, 200, 'assetsSuper_Incubator.png'),
-(4, 'Premium Battle Pass x1', 100, 100, 'assetsPremium_Battle_Pass.png'),
-(5, 'Premium Battle Pass x3', 30, 250, 'assetsPremium_Battle_Pass.png'),
-(6, 'Remote Raid Pass x1', 5, 195, 'assetsRemote_Raid_Pass.png'),
-(7, 'Remote Raid Pass x3', 1, 525, 'assetsRemote_Raid_Pass.png'),
-(8, 'Poffin', 100, 100, 'assetsPoffin.png'),
-(9, 'Pokeball x20', 25, 100, 'assetsPokeball.png'),
-(10, 'Pokeball x100', 15, 460, 'assetsPokeball100.png'),
-(11, 'Pokeball x200', 5, 800, 'assetsPokeball200.png'),
-(12, 'Incense x1', 100, 80, 'assetsIncense.png'),
-(13, 'Incense x8', 15, 500, 'assetsIncense8.png'),
-(14, 'Incense x25', 5, 1250, 'assetsIncense25.png'),
-(15, 'Star Piece x1', 100, 80, 'assetsStar_Piece.png'),
-(16, 'Star Piece x8', 20, 640, 'assetsStar_Piece8.png'),
-(17, 'Max Potion x10', 20, 200, 'assetsMax_Potion10.png'),
-(18, 'Max Revive x6', 30, 180, 'assetsMax_Revive6.png'),
-(19, 'Lucky Egg x1', 150, 80, 'assetsLucky_Egg.png'),
-(20, 'Lucky Egg x8', 15, 500, 'assetsLucky_Egg8.png'),
-(21, 'Lucky Egg x25', 5, 1250, 'assetsLucky_Egg25.png'),
-(22, 'Lure Module x1', 50, 100, 'assetsLure_Module.png'),
-(23, 'Lure Module x8', 10, 680, 'assetsLure_Module8.png'),
-(24, 'Glacial Lure Module x1', 50, 200, 'assetsGlacial_Lure_Module.png'),
-(25, 'Mossy Lure Module x1', 50, 200, 'assetsMossy_Lure_Module.png'),
-(26, 'Magnetic Lure Module x1', 50, 200, 'assetsMagnetic_Lure_Module.png'),
-(27, 'Rainy Lure Module x1', 50, 200, 'assetsRainy_Lure_Module.png'),
-(28, 'Bag Upgrade', 63, 200, 'assetsBag_Upgrade.png'),
-(29, 'Pokemon Storage Upgrade', 118, 200, 'assetsStorage_Upgrade.png'),
-(30, 'Postcard Storage Upgrade', 13, 100, 'assetsPostcard_Storage_Upgrade.png'),
-(31, 'New Trainer Box', 100, 75, 'assetsNew_Trainer_Box.png'),
-(32, 'Beginner Box', 50, 150, 'assetsBeginner_Box.png'),
-(33, 'Trainee Box', 40, 215, 'assetsTrainee_Box.png'),
-(34, 'Voyager Box', 20, 1485, 'assetsVoyager_Box.png'),
-(35, 'Coin Handful - 100', 100, 29, 'assetsCoin_Handful.png'),
-(36, 'Coin Stack - 500', 100, 249, 'assetsCoin_Stack.png'),
-(37, 'Coin Pouch - 1200', 100, 499, 'assetsCoin_Pouch.png'),
-(38, 'Coin Bucket - 2500', 100, 999, 'assetsCoin_Bucket.png'),
-(39, 'Coin Box - 5200', 100, 1990, 'assetsCoin_Box.png'),
-(40, 'Coin Heap - 14500', 100, 4990, 'assetsCoin_Heap.png');
+INSERT INTO `items` (`id`, `name`, `stock`, `price`, `image`, `description`, `category`) VALUES
+(0, 'Pokeball x20', 25, 100, 'assets\\Pokeball.png', 'Catch Pokemons', 'Pokeballs'),
+(1, 'Pokeball x100', 15, 460, 'assets\\Pokeball100.png', 'Catch Pokemons', 'Pokeballs'),
+(2, 'Pokeball x200', 5, 800, 'assets\\Pokeball200.png', 'Catch Pokemons', 'Pokeballs'),
+(3, 'Incubator', 100, 150, 'assets\\Incubator_Limited.png', 'Hatch Pokemons', 'Incubators'),
+(4, 'Super Incubator', 50, 200, 'assets\\Super_Incubator.png', 'Hatch Pokemons', 'Incubators'),
+(5, 'Premium Battle Pass x1', 100, 100, 'assets\\Premium_Battle_Pass.png', 'Participate in raids', 'Battle Passes'),
+(6, 'Premium Battle Pass x3', 30, 250, 'assets\\Premium_Battle_Pass.png', 'Participate in raids', 'Battle Passes'),
+(7, 'Remote Raid Pass x1', 5, 195, 'assets\\Remote_Raid_Pass.png', 'Remotely participate in raids', 'Battle Passes'),
+(8, 'Remote Raid Pass x3', 1, 525, 'assets\\Remote_Raid_Pass.png', 'Remotely participate in raids', 'Battle Passes'),
+(9, 'Incense x1', 100, 80, 'assets\\Incense.png', 'Lure Pokemons to you', 'Incense'),
+(10, 'Incense x8', 15, 500, 'assets\\Incense8.png', 'Lure Pokemons to you', 'Incense'),
+(11, 'Incense x25', 5, 1250, 'assets\\Incense25.png', 'Lure Pokemons to you', 'Incense'),
+(12, 'Max Potion x10', 20, 200, 'assets\\Max_Potion10.png', 'Restore Pokemon hp', 'Medicine'),
+(13, 'Max Revive x6', 30, 180, 'assets\\Max_Revive6.png', 'Revive Pokemon', 'Medicine'),
+(14, 'Lucky Egg x1', 150, 80, 'assets\\Lucky_Egg.png', 'Hatches into a Pokemon', 'Eggs'),
+(15, 'Lucky Egg x8', 15, 500, 'assets\\Lucky_Egg8.png', 'Hatches into a Pokemon', 'Eggs'),
+(16, 'Lucky Egg x25', 5, 1250, 'assets\\Lucky_Egg25.png', 'Hatches into a Pokemon', 'Eggs'),
+(17, 'Lure Module x1', 50, 100, 'assets\\Lure_Module.png', 'Lures Pokemon to a spot', 'Lures'),
+(18, 'Lure Module x8', 10, 680, 'assets\\Lure_Module8.png', 'Lures Pokemon to a spot', 'Lures'),
+(19, 'Glacial Lure Module x1', 50, 200, 'assets\\Glacial_Lure_Module.png', 'Lures Pokemon to a spot', 'Lures'),
+(20, 'Mossy Lure Module x1', 50, 200, 'assets\\Mossy_Lure_Module.png', 'Lures Pokemon to a spot', 'Lures'),
+(21, 'Magnetic Lure Module x1', 50, 200, 'assets\\Magnetic_Lure_Module.png', 'Lures Pokemon to a spot', 'Lures'),
+(22, 'Rainy Lure Module x1', 50, 200, 'assets\\Rainy_Lure_Module.png', 'Lures Pokemon to a spot', 'Lures'),
+(23, 'Bag Upgrade', 63, 200, 'assets\\Bag_Upgrade.png', 'Increase Bag Capacity', 'Upgrades'),
+(24, 'Pokemon Storage Upgrade', 118, 200, 'assets\\Storage_Upgrade.png', 'Increase Pokemon Storage', 'Upgrades'),
+(25, 'Postcard Storage Upgrade', 13, 100, 'assets\\Postcard_Storage_Upgrade.png', 'Increase Postcard Storage', 'Upgrades'),
+(26, 'New Trainer Box', 100, 75, 'assets\\New_Trainer_Box.png', 'Assorted Items', 'Boxes'),
+(27, 'Beginner Box', 50, 150, 'assets\\Beginner_Box.png', 'Assorted Items', 'Boxes'),
+(28, 'Trainee Box', 40, 215, 'assets\\Trainee_Box.png', 'Assorted Items', 'Boxes'),
+(29, 'Voyager Box', 20, 1485, 'assets\\Voyager_Box.png', 'Assorted Items', 'Boxes'),
+(30, 'Team Medallion', 1, 1000, 'assets\\Team_Medallion.png', 'Change teams(One-time use)', 'Others'),
+(31, 'Poffin', 100, 100, 'assets\\Poffin.png', 'Boost Pokemon\'s Conditions', 'Others'),
+(32, 'Star Piece x1', 100, 80, 'assets\\Star_Piece.png', 'Stardust boost', 'Others'),
+(33, 'Star Piece x8', 20, 640, 'assets\\Star_Piece8.png', 'Stardust boost', 'Others'),
+(34, 'Coin Handful', 100, 29, 'assets\\Coin_Handful.png', '100 Pokecoins', 'Coins'),
+(35, 'Coin Stack', 100, 249, 'assets\\Coin_Stack.png', '500 Pokecoins', 'Coins'),
+(36, 'Coin Pouch', 100, 499, 'assets\\Coin_Pouch.png', '1200 Pokecoins', 'Coins'),
+(37, 'Coin Bucket', 100, 999, 'assets\\Coin_Bucket.png', '2500 Pokecoins', 'Coins'),
+(38, 'Coin Box', 100, 1990, 'assets\\Coin_Box.png', '5200 Pokecoins', 'Coins'),
+(39, 'Coin Heap', 100, 4990, 'assets\\Coin_Heap.png', '14500 Pokecoins', 'Coins');
 
 -- --------------------------------------------------------
 
